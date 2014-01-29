5a9777261cbcbbd94dd72ff80aa646adbf8dd163
Andrea Cappelli <a.cappelli@gmail.com>
2014-01-29 14:49:25 +0100

    Move from svn

---
+++ manifests/init.pp
@@ -0,0 +1,26 @@
+# == class swftools
+#
+#  Install collection of utilities for SWF file manipulation/creation
+#
+# === Params
+#
+# === Examples
+#
+class swftools {
+    case $lsbdistcodename {
+        "lucid": {
+           package {
+                "swftools": 
+                    ensure  => present,
+                    require => [ Class["apt"] ],
+            }
+        }
+
+        default : {
+            package {
+                "swftools": 
+                    ensure => present;
+            }
+        }
+    }
+}
